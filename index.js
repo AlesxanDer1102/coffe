6b15100850ce378d517a9d35f3ee00f3e3efbbb0
AlesxanDer1102 <diego.orrego.t@uni.pe>
2025-07-06 02:23:33 -0500

    feat: agregar funcionalidad de retiro y balance en el contrato

--- index.js
+++ index.js
@@ -3,13 +3,16 @@ import {
   custom,
   createPublicClient,
   parseEther,
-  defineChain
+  defineChain,
+  formatEther
 } from "https://esm.sh/viem"
 import { abi, contractAddress } from "./constants.js"
 
 const connectButton = document.getElementById("connectButton")
 const fundButton = document.getElementById("fundButton")
 const ethAmountInput = document.getElementById("ethAmount")
+const balanceButton = document.getElementById("balanceButton")
+const withdrawButton = document.getElementById("withdrawButton")
 
 let walletClient
 let publicClient
@@ -56,6 +59,32 @@ async function fund() {
   }
 }
 
+async function withdraw() {
+  if (typeof window.ethereum !== "undefined") {
+    walletClient = createWalletClient({
+      transport: custom(window.ethereum)
+    })
+    const [connectedAccount] = await walletClient.requestAddresses()
+    const currentChain = await getCurrentChain(walletClient)
+    publicClient = createPublicClient({
+      transport: custom(window.ethereum)
+    })
+
+    const { request } = await publicClient.simulateContract({
+      address: contractAddress,
+      abi: abi,
+      functionName: "withdraw",
+      account: connectedAccount,
+      chain: currentChain
+    })
+
+    const hash = await walletClient.writeContract(request)
+    console.log(`Withdraw transaction hash: ${hash}`)
+  } else {
+    connectButton.innerText = "Please install MetaMask"
+  }
+}
+
 async function getCurrentChain(client) {
   const chainId = await client.getChainId()
   const currentChain = defineChain({
@@ -75,5 +104,18 @@ async function getCurrentChain(client) {
   return currentChain
 }
 
+async function getBalance() {
+  if (typeof window.ethereum !== "undefined") {
+    publicClient = createPublicClient({
+      transport: custom(window.ethereum)
+    })
+    const balance = await publicClient.getBalance({
+      address: contractAddress
+    })
+    console.log(`Balance: ${formatEther(balance)} Ether`)
+  }
+}
+withdrawButton.onclick = withdraw
+balanceButton.onclick = getBalance
 connectButton.onclick = connect
 fundButton.onclick = fund
